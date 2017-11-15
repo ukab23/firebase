ecfa6718a1cb461490b4ecb925e6906e801cd764
Akash <ukab23@gmail.com>
2017-11-15 09:18:34 +0530

    Added Authentication

--- app/src/main/java/com/example/akash/firebase/ProfileActivity.java
+++ app/src/main/java/com/example/akash/firebase/ProfileActivity.java
@@ -5,16 +5,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ProfileActivity extends AppCompatActivity implements View.OnClickListener{
 
     private FirebaseAuth firebaseAuth;
     private TextView textViewUserEmail;
     private Button buttonLogOut;
+    private EditText editTextName;
+    private EditText editTextAddress;
+    private Button buttonSave;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +31,13 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
 
         textViewUserEmail = (TextView) findViewById(R.id.textViewUserEmail);
         buttonLogOut = (Button) findViewById(R.id.buttonLogOut);
+        editTextName = (EditText) findViewById(R.id.editTextName);
+        editTextAddress = (EditText) findViewById(R.id.editTextAddress);
+        buttonSave = (Button) findViewById(R.id.buttonSave);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference();
         firebaseAuth = FirebaseAuth.getInstance();
+
         if(firebaseAuth.getCurrentUser() == null){
             finish();
             startActivity(new Intent(getApplicationContext(),LoginActivity.class));
@@ -32,6 +46,19 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
         textViewUserEmail.setText("Welcome "+user.getEmail());
 
         buttonLogOut.setOnClickListener(this);
+        buttonSave.setOnClickListener(this);
+    }
+
+    private void saveUserInformation(){
+        String name = editTextName.getText().toString().trim();
+        String address = editTextAddress.getText().toString().trim();
+
+        userInformation userinformation = new userInformation(name,address);
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        databaseReference.child(user.getUid()).setValue(userinformation);
+        Toast.makeText(getApplicationContext(),"Information Saved",Toast.LENGTH_SHORT).show();
+        editTextName.setText("");
+        editTextAddress.setText("");
     }
 
     @Override
@@ -40,6 +67,10 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
             firebaseAuth.signOut();
             finish();
             startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+        }
+
+        if(view == buttonSave){
+            saveUserInformation();
         }
     }
 }
---
+++ app/src/main/java/com/example/akash/firebase/userInformation.java
@@ -0,0 +1,20 @@
+package com.example.akash.firebase;
+
+/**
+ * Created by akash on 15-11-2017.
+ */
+
+public class userInformation {
+
+    public String name;
+    public String address;
+
+    public userInformation(){
+
+    }
+
+    public userInformation(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+}
